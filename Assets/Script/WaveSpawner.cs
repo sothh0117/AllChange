dde188d1364bc5b937fb294390228b614a4e71c8
sothh0117 <ljss1211@gmail.com>
2024-04-08 01:10:38 +0900

    enemy wave waypoint
    게임 유형 타워 디펜스로 바꾸고 enemy, wave, waypoint 스크립트 추가

---
+++ Assets/Script/WaveSpawner.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public Transform spawnPoint;
+    public float timeBetweenWaves = 5f;
+
+    private float countdown = 2f;
+    private int waveNumber = 1;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(countdown <= 0f)
+        {
+            SpawnWave();
+            countdown = timeBetweenWaves;
+        }
+        countdown -= Time.deltaTime;
+    }
+
+    private void SpawnWave()
+    {
+        for(int i = 0; i < waveNumber; i++)
+        {
+            SpawnEnemy();
+        }
+        waveNumber++;
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+}
